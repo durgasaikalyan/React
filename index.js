ea0b60eeb6c909ea2cc299c01ce0079bd89921f4
durgasaikalyan <durgasaikalyanccbp@gmail.com>
2022-12-08 16:06:01 +0000

    your message

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+  //   Write your code here.
+  <div>
+    <h1 className="greeting">Super Over League</h1>;
+    <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
